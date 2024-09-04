d30bf1771cd188254f75f392f3c96bd766bc953d
fallmpro <113703061+fallmpro@users.noreply.github.com>
2024-09-04 16:49:21 +0200

    Add files via upload
    Depôt Voyage.java

---
+++ EasyLine/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Voyage dakar = new Voyage();
+        dakar.setDestination("Paname");
+        dakar.setPrix(2000);
+        dakar.setDuree(24);
+        System.out.println("La destination est " + dakar.getDestination());
+
+        System.out.println("La prix est " + dakar.getPrix() + "€");
+
+        System.out.println("La Duree est " + dakar.getDuree() + "H");
+    }
+}
---
+++ EasyLine/src/Voyage.java
@@ -0,0 +1,32 @@
+public class Voyage {
+    private int duree;
+    private String destination;
+            private double prix;
+
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int id) {
+        this.duree = id;
+    }
+}
+
+
