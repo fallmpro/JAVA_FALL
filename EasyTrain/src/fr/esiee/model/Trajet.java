fc2fcbb203013f7d43e3141d631652d9d1a5cb73
momof <mouhamad.fall.pro@gmail.com>
2024-09-24 13:57:40 +0200

    Création getters & setters Trajet.java

--- EasyTrain/src/fr/esiee/model/Trajet.java
+++ EasyTrain/src/fr/esiee/model/Trajet.java
@@ -1,4 +1,42 @@
 package fr.esiee.model;
 
+
+import java.time.LocalDateTime;
+
 public class Trajet {
+    private String code;
+    private LocalDateTime tempsDepart;
+    private LocalDateTime tempsArrivee;
+
+
+    public Trajet(String code, LocalDateTime tempsDepart, LocalDateTime tempsArrivee) {
+        this.code = code;
+        this.tempsDepart = tempsDepart;
+        this.tempsArrivee = tempsArrivee;
+
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public LocalDateTime getTempsDepart() {
+        return tempsDepart;
+    }
+
+    public void setTempsDepart(LocalDateTime tempsDepart) {
+        this.tempsDepart = tempsDepart;
+    }
+
+    public LocalDateTime getTempsArrivee() {
+        return tempsArrivee;
+    }
+
+    public void setTempsArrivee(LocalDateTime tempsArrivee) {
+        this.tempsArrivee = tempsArrivee;
+    }
 }
