faed3cc335f0a94955583014216e7ce105183f27
fallmpro <113703061+fallmpro@users.noreply.github.com>
2025-01-10 16:29:37 +0100

    Delete Projet_JUNIT5 directory

--- Projet_JUNIT5/src/main/java/fr/esiee/Main.java
+++
@@ -1,9 +0,0 @@
-package fr.esiee;
-
-public class Main {
-    public static void main(String[] args) {
-        Devise montant = new Devise(100, "euro");
-        System.out.println("Montant: " + montant.getQuantite() + " " + montant.getMonnaie());
-    }
-}
-
